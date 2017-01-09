3fd94c060ef796b75b27d6459479559d701c140b
Peter Cunha <petercunha8@gmail.com>
2017-01-09 14:22:02 -0500

    Clean code base, add comments, add command-line args, add meaningful filenames for downloaded songs

--- bc-rip.py
+++ bc-rip.py
@@ -3,32 +3,47 @@ import os
 import urllib2
 import json
 
-URL = "https://pogomix.bandcamp.com/album/kindred-shadow"
-
 def main():
+	# Check for command line args
+	if len(sys.argv) == 1:
+		exit("\nYou didn't provide a URL!\nusage: python bc-rip.py <bandcamp album URL>\n")
+	else:
+		startRip(sys.argv[1])
+	pass
+
+# Rips 128-bitrate MP3's from bandcamp album URL
+def startRip(URL):
+
+	# Grab HTML source of Bandcamp album page
 	req = urllib2.Request(URL)
 	response = urllib2.urlopen(req)
 	source = response.read()
 
-	arr = source.split('"mp3-128":"//')
+	# Parse HTML into meaningful arrays of data
+	download_arr = source.split('"mp3-128":"//')
+	name_arr = source.split('"title":"')
 
-	# dirname format: Artist - Album name
-	dirname = URL.split('://')[1].split('.bandcamp')[0] + " - " + URL.split('album/')[1].replace("-", " ")
+	# dirname format: Artist - Album Name
+	dirname = (URL.split('://')[1].split('.bandcamp')[0] + " - " + URL.split('album/')[1].replace("-", " ")).title()
 	
+	# Create dir to download music if it doesn't exist
 	if not os.path.exists(dirname):
 		os.makedirs(dirname)
 
-	for x in xrange(1,10):
-		download("https://" + arr[x].split('"')[0], dirname + "/" + str(x))
+	# Download each file with some split witchcraft
+	for x in xrange(len(download_arr)-1):
+		download("https://" + download_arr[x+1].split('"')[0], 
+				 dirname + "/" + str(x+1) + ".) " + name_arr[x+2].split('"')[0])
 		pass
 
+# Downloads file at URL to specified location and filename
 def download(url, filename):
 	mp3file = urllib2.urlopen(url)
 	with open(filename + ".mp3",'wb') as output:
 		output.write(mp3file.read())
 	pass
 
-
+# Finds string between two other strings
 def find_between( s, first, last ):
     try:
         start = s.index( first ) + len( first )
